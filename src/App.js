4182eca9ae170ed3884cef397e49efc744ec5d01
Samuel Rowan <samuel@samuelrowan.com>
2020-01-25 14:27:19 -0600

    Moved useEffect to App
    Still need to share the character state from App.js to both CharacterList and Character through props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { useEffect, useState } from "react";
 import Header from "./components/Header.js";
 import { Route, Link } from "react-router-dom";
+import axios from "axios";
 
 import CharacterList from "./components/CharacterList";
 import Character from "./components/Character";
 
 export default function App() {
   const [ characters, setCharacters ] = useState([]);
-
+  useEffect(() => {
+    // TODO: Add API Request here - must run in `useEffect`
+    axios
+      .get(`https://rickandmortyapi.com/api/character/`)
+      .then(response => {
+        // setCharacters(response.data);
+        setCharacters(response.data.results);
+      })
+      .catch(err => {
+        console.log("there's an issue", err);
+      });
+    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+  }, []);
   return (
     <main>
       <Header />
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,23 +1,9 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
   const [ characters, setCharacters ] = useState([]);
-  useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    axios
-      .get(`https://rickandmortyapi.com/api/character/`)
-      .then(response => {
-        // setCharacters(response.data);
-        setCharacters(response.data.results);
-      })
-      .catch(err => {
-        console.log("there's an issue", err);
-      });
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
   console.log("characters2", characters);
   return (
      <section className="character-list">
